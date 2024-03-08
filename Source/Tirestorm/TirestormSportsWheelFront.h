96ef566e8b00429cb564a28253b4e2b5ad1bb4de
PJWoods <patrik.skoog83@gmail.com>
2024-03-08 20:09:37 +0100

    Add files via upload

---
+++ Source/Tirestorm/TirestormSportsWheelFront.h
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TirestormWheelFront.h"
+#include "TirestormSportsWheelFront.generated.h"
+
+/**
+ *  Front wheel definition for Sports Car.
+ */
+UCLASS()
+class TIRESTORM_API UTirestormSportsWheelFront : public UTirestormWheelFront
+{
+	GENERATED_BODY()
+	
+public:
+
+	UTirestormSportsWheelFront();
+};
---
+++ Source/Tirestorm/TirestormSportsWheelRear.h
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TirestormWheelRear.h"
+#include "TirestormSportsWheelRear.generated.h"
+
+/**
+ *  Rear wheel definition for Sports Car.
+ */
+UCLASS()
+class TIRESTORM_API UTirestormSportsWheelRear : public UTirestormWheelRear
+{
+	GENERATED_BODY()
+	
+public:
+
+	UTirestormSportsWheelRear();
+};
